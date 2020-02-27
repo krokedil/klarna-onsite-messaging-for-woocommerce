19aca0593956d31ee4d1fe896d20cf170f0a14a6
Niklas Högefjord <niklas@krokedil.se>
2020-02-27 15:27:45 +0100

    Deprecated notices

--- classes/class-klarna-onsite-messaging-cart-page.php
+++ classes/class-klarna-onsite-messaging-cart-page.php
@@ -55,7 +55,7 @@ class Klarna_OnSite_Messaging_Cart_Page {
 	 */
 	public function is_enabled() {
 		$store_base_location = apply_filters( 'klarna_onsite_messaging_store_location', wc_get_base_location()['country'] );
-		$customer_location   = apply_filters( 'klarna_onsite_messaging_customer_location', WC()->customer->get_country() );
+		$customer_location   = apply_filters( 'klarna_onsite_messaging_customer_location', WC()->customer->get_billing_country() );
 		if ( $store_base_location == $customer_location ) {
 			$this->settings = Klarna_OnSite_Messaging_For_WooCommerce::get_settings();
 			if ( ! isset( $this->settings['onsite_messaging_enabled_cart'] ) || 'yes' === $this->settings['onsite_messaging_enabled_cart'] ) {
