0deff3b983018f88449e2966380164a8f4ec53a4
Niklas Högefjord <niklas@krokedil.se>
2020-03-03 21:32:49 +0100

    Removed "production" from url to enqueue Klarna js file

--- klarna-onsite-messaging-for-woocommerce.php
+++ klarna-onsite-messaging-for-woocommerce.php
@@ -256,15 +256,15 @@ class Klarna_OnSite_Messaging_For_WooCommerce {
 		}
 
 		if ( 'yes' === $settings['testmode'] ) {
-			$environment = 'playground';
+			$environment = 'playground.';
 		} else {
-			$environment = 'production';
+			$environment = '';
 		}
 		if ( is_product() || is_cart() ) {
 			if( ! empty( $this->data_client_id ) ) {
-				wp_enqueue_script( 'klarna-onsite-messaging', 'https://' . $region . '.' . $environment . '.klarnaservices.com/lib.js', array( 'jquery' ), WC_KLARNA_ONSITE_MESSAGING_VERSION, true );
+				wp_enqueue_script( 'klarna-onsite-messaging', 'https://' . $region . '.' . $environment . 'klarnaservices.com/lib.js', array( 'jquery' ), WC_KLARNA_ONSITE_MESSAGING_VERSION, true );
 			} elseif( ! empty( $uci ) ) {
-				wp_enqueue_script( 'onsite_messaging_script', 'https://' . $region . '.' . $environment . '.klarnaservices.com/merchant.js?uci=' . $uci . '&country=' . wc_get_base_location()['country'], array( 'jquery' ) );
+				wp_enqueue_script( 'onsite_messaging_script', 'https://' . $region . '.' . $environment . 'klarnaservices.com/merchant.js?uci=' . $uci . '&country=' . wc_get_base_location()['country'], array( 'jquery' ) );
 			}
 
 			wp_register_script( 'klarna_onsite_messaging', plugins_url( '/assets/js/klarna-onsite-messaging.js', __FILE__ ), array( 'jquery' ), WC_KLARNA_ONSITE_MESSAGING_VERSION );
