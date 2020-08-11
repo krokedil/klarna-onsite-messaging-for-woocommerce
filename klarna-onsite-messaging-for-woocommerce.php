02f862f042d36dcf4cad8765a663eed3d1acb688
Niklas Högefjord <niklas@krokedil.se>
2020-08-11 12:43:43 +0200

    Version update (1.30)

--- klarna-onsite-messaging-for-woocommerce.php
+++ klarna-onsite-messaging-for-woocommerce.php
@@ -5,12 +5,12 @@
  * Description: Provides Klarna On-Site Messaging for WooCommerce
  * Author: krokedil, klarna
  * Author URI: https://krokedil.se/
- * Version: 1.2.2
+ * Version: 1.3.0
  * Text Domain: klarna-onsite-messaging-for-woocommerce
  * Domain Path: /languages
  *
- * WC requires at least: 3.0
- * WC tested up to: 4.2.0
+ * WC requires at least: 3.8
+ * WC tested up to: 4.3.2
  *
  * @package Klarna_OnSite_Messaging
  *
@@ -31,7 +31,7 @@
  */
 
 // Definitions.
-define( 'WC_KLARNA_ONSITE_MESSAGING_VERSION', '1.2.2' );
+define( 'WC_KLARNA_ONSITE_MESSAGING_VERSION', '1.3.0' );
 define( 'WC_KLARNA_ONSITE_MESSAGING_PLUGIN_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 define( 'WC_KLARNA_ONSITE_MESSAGING_PLUGIN_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
 
