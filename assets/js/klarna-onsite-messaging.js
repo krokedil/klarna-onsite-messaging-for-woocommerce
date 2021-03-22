e6f1fdaa31d987414b17058ffcd5b6d59a7ae96a
Michael Bengtsson <michael@krokedil.se>
2021-03-22 22:13:16 +0100

    Add support for updating cart fragment

--- assets/js/klarna-onsite-messaging.js
+++ assets/js/klarna-onsite-messaging.js
@@ -3,7 +3,7 @@ jQuery( function($) {
 		update_iframe: function() {
 			window.kudt = window.kudt || [];
 			window.kudt.push({eventName: 'refresh-placements'});
-			$('klarna-placement').show();			
+			$('klarna-placement').show();
 		},
 
 		check_variable: function() {
@@ -14,7 +14,16 @@ jQuery( function($) {
 			return true;
 		},
 
-		update_total: function( variation ) {
+		update_total_cart: function() {
+			let price = Math.round( $("#kosm_cart_total").val() * 100 );
+			$("klarna-placement").each( function() {
+				$( this ).attr("data-total_amount", price );
+				$( this ).attr("data-purchase-amount", price );
+			});
+			klarna_onsite_messaging.update_iframe();
+		},
+
+		update_total_variation: function( variation ) {
 			let price = Math.round( variation.display_price * 100 );
 			$("klarna-placement").each( function() {
 				$( this ).attr("data-total_amount", price );
@@ -24,9 +33,6 @@ jQuery( function($) {
 		},
 
 		init: function() {
-			$(document.body).on("updated_cart_totals", function () {
-				klarna_onsite_messaging.update_iframe();
-			});
 			$(document).ready( function() {
 				if( false === klarna_onsite_messaging.check_variable ) {
 					$('klarna-placement').hide();
@@ -34,9 +40,12 @@ jQuery( function($) {
 					$('klarna-placement').show();
 				}
 			});
-		
+			
+			$(document.body).on("updated_cart_totals", function () {
+				klarna_onsite_messaging.update_total_cart();
+			});
 			$(document).on( 'found_variation', function( e, variation ) {
-				klarna_onsite_messaging.update_total( variation );
+				klarna_onsite_messaging.update_total_variation( variation );
 			});
 		}
 	}
