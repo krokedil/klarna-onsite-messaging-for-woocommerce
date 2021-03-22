f4fc30cc0f818fbeabda80408e31b22da6079b82
Michael Bengtsson <michael@krokedil.se>
2021-03-22 22:24:58 +0100

    Add support for automatic pricing on shortcodes added to product or cart pages if none is added

--- classes/class-klarna-onsite-messaging-shortcode.php
+++ classes/class-klarna-onsite-messaging-shortcode.php
@@ -23,6 +23,10 @@ class Klarna_OnSite_Messaging_Shortcode {
 	 * @return string
 	 */
 	public function osm_shortcode( $atts ) {
+		// If no price is set, maybe set the one for the product or cart page.
+		if ( ! isset( $atts['data-purchase-amount'] ) ) {
+			$atts = $this->set_price( $atts );
+		}
 		if ( ! is_admin() ) {
 			do_action( 'osm_shortcode_added' );
 			$atts = shortcode_atts(
@@ -30,13 +34,37 @@ class Klarna_OnSite_Messaging_Shortcode {
 					'data-key'             => 'homepage-promotion-wide',
 					'data-theme'           => '',
 					'data-purchase-amount' => '',
-				), $atts
+				),
+				$atts
 			);
 			ob_start();
 			kosm_klarna_placement( $atts );
 			$html = ob_get_clean();
 			return $html;
 		}
+	}
+
+	/**
+	 * Get the price if we can for a product or cart page.
+	 *
+	 * @param array $atts The attributes for the shortcode.
+	 * @return array
+	 */
+	public function set_price( $atts ) {
+		$price = '';
+		if ( is_product() ) {
+			global $product;
+			if ( $product->is_type( 'variable' ) ) {
+				$price = $product->get_variation_price( 'min' ) * 100;
+			} else {
+				$price = wc_get_price_to_display( $product ) * 100;
+			}
+		} elseif ( is_cart() ) {
+			$price = WC()->cart->get_total( 'klarna_onsite_messaging' ) * 100;
+		}
+
+		$atts['data-purchase-amount'] = $price;
+		return $atts;
 	}
 }
 new Klarna_OnSite_Messaging_Shortcode();
